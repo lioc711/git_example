c9476c083a8c7a0902a86b30426282776eb63ab3
rotemlev <37338583+rotemlev@users.noreply.github.com>
2025-04-04 15:22:13 +0300

    Add files via upload

---
+++ iac/main.tf
@@ -0,0 +1,76 @@
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "random_string" "suffix" {
+  length  = 5
+  special = false
+}
+
+resource "azurerm_container_registry" "acr" {
+  name                = "myaksregistry${random_string.suffix.result}"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  sku                 = "Basic"
+  admin_enabled       = false
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "aksVnet"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  address_space       = ["10.0.0.0/16"]
+}
+
+resource "azurerm_subnet" "aks_subnet" {
+  name                 = "aksSubnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = "myPrivateAksCluster"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  dns_prefix          = "aksdns"
+
+  default_node_pool {
+    name           = "default"
+    node_count     = 2
+    vm_size        = "Standard_DS2_v2"
+    vnet_subnet_id = azurerm_subnet.aks_subnet.id
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  network_profile {
+    network_plugin     = "azure"
+    load_balancer_sku  = "standard"
+    service_cidr       = "10.0.2.0/24"
+    dns_service_ip     = "10.0.2.10"
+    docker_bridge_cidr = "172.17.0.1/16"
+    outbound_type      = "userDefinedRouting"
+  }
+
+  api_server_access_profile {
+    enable_private_cluster = true
+  }
+
+  role_based_access_control {
+    enabled = true
+  }
+
+  depends_on = [
+    azurerm_subnet.aks_subnet
+  ]
+}
+
+resource "azurerm_role_assignment" "aks_acr_pull" {
+  principal_id         = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
+  role_definition_name = "AcrPull"
+  scope                = azurerm_container_registry.acr.id
+}
